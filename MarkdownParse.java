3160632d5e53a886bf1373c1e89a91229022927e
kevinhuang-ucsd <zih020@ucsd.edu>
2022-04-15 09:08:55 -0700

    fixed the no link situation

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -16,6 +16,9 @@ public class MarkdownParse {
             int closeBracket = markdown.indexOf("]", openBracket);
             int openParen = markdown.indexOf("(", closeBracket);
             // int closeParen = markdown.indexOf(")", openParen);
+            if (openParen == -1) {
+                break;
+            }
             int countOpenBracket = 1;
             int closeParen = -1;
             for (int i = openParen + 1; i < markdown.length(); i ++) {
