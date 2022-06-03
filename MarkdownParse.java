95e4cf0a30988e01e7d108951f6c9ab625bae4c3
kevinhuang-ucsd <zih020@ucsd.edu>
2022-06-03 13:00:25 -0700

    recover MarkdownParse.java

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -1,3 +1,5 @@
+
+   
 //https://howtodoinjava.com/java/io/java-read-file-to-string-examples/
 
 import java.io.IOException;
@@ -8,11 +10,45 @@ import java.util.ArrayList;
 public class MarkdownParse {
 
     public static ArrayList<String> getLinks(String markdown) {
-        Parser parser = Parser.builder().build();
-        Node node = parser.parse(markdown);
-        LinkVisitor visitor = new LinkVisitor();
-        node.accept(visitor);
-        return visitor.links;
+        ArrayList<String> toReturn = new ArrayList<>();
+        // find the next [, then find the ], then find the (, then read link upto next )
+        int currentIndex = 0;
+        while(currentIndex < markdown.length()) {
+            
+            int openBracket = markdown.indexOf("[", currentIndex);
+            int closeBracket = markdown.indexOf("]", openBracket);
+            int openParen = markdown.indexOf("(", closeBracket);
+            if (openParen == -1 || closeBracket == -1 || openBracket == -1) {
+                break;
+            }
+            if (closeBracket != markdown.length() - 1 && markdown.substring(closeBracket + 1, closeBracket + 2).equals("[")) {
+                openBracket = closeBracket + 1;
+                closeBracket = markdown.indexOf("]", openBracket);
+            }
+            int countOpenBracket = 1;
+            int closeParen = -1;
+            for (int i = openParen + 1; i < markdown.length(); i ++) {
+                if(markdown.substring(i, i+1).equals("(")) {
+                    countOpenBracket ++;
+                } else if (markdown.substring(i, i+1).equals(")")) {
+                    countOpenBracket --;
+                } 
+                if (countOpenBracket == 0) {
+                    closeParen = i;
+                    break;
+                }
+            }
+            String beforeOpenBracket = "";
+            if (openBracket != 0) {
+                beforeOpenBracket = markdown.substring(openBracket - 1, openBracket);
+            }
+            if (!beforeOpenBracket.equals("!") && (closeBracket == openParen - 1) && (closeBracket != openBracket + 1)) {
+                toReturn.add(markdown.substring(openParen + 1, closeParen));
+            }
+            currentIndex = closeParen + 1;
+        }
+
+        return toReturn;
     }
 
     public static void main(String[] args) throws IOException {
@@ -20,14 +56,5 @@ public class MarkdownParse {
         String content = Files.readString(fileName);
         ArrayList<String> links = getLinks(content);
 	    System.out.println(links);
-    }
-}
-
-class LinkVisitor extends AbstractVisitor {
-    ArrayList<String> links = new ArrayList<>();
-    @Override
-    public void visit(Link link) {
-        links.add(link.getDestination());
-        visitChildren(link);
     }
 }
