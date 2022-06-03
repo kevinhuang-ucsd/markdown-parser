5f8f4f7aebbafcb0b7750dc974b1b7fefa89f771
kevinhuang-ucsd <zih020@ucsd.edu>
2022-06-03 08:45:58 -0700

    add WordCountVisitor

--- TryCommonMark.java
+++ TryCommonMark.java
@@ -8,5 +8,19 @@ class TryCommonMark {
         Node document = parser.parse("This is *Sparta*");
         HtmlRenderer renderer = HtmlRenderer.builder().build();
         System.out.println(renderer.render(document));
+        Node node = parser.parse("Example\n=======\n\nSome more text");
+        WordCountVisitor visitor = new WordCountVisitor();
+        node.accept(visitor);
+        System.out.println(visitor.wordCount);
+    }
+}
+
+class WordCountVisitor extends AbstractVisitor {
+    int wordCount = 0;
+
+    @Override
+    public void visit(Text text) {
+        wordCount += text.getLiteral().split("\\W+").length;
+        visitChildren(text);
     }
 }
