e0b9dcb9be06e04bc093ef0cdb21d2a2aa93ce02
kevinhuang-ucsd <zih020@ucsd.edu>
2022-06-03 08:52:31 -0700

    update MarkdownParse

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -8,45 +8,11 @@ import java.util.ArrayList;
 public class MarkdownParse {
 
     public static ArrayList<String> getLinks(String markdown) {
-        ArrayList<String> toReturn = new ArrayList<>();
-        // find the next [, then find the ], then find the (, then read link upto next )
-        int currentIndex = 0;
-        while(currentIndex < markdown.length()) {
-            
-            int openBracket = markdown.indexOf("[", currentIndex);
-            int closeBracket = markdown.indexOf("]", openBracket);
-            int openParen = markdown.indexOf("(", closeBracket);
-            if (openParen == -1 || closeBracket == -1 || openBracket == -1) {
-                break;
-            }
-            if (closeBracket != markdown.length() - 1 && markdown.substring(closeBracket + 1, closeBracket + 2).equals("[")) {
-                openBracket = closeBracket + 1;
-                closeBracket = markdown.indexOf("]", openBracket);
-            }
-            int countOpenBracket = 1;
-            int closeParen = -1;
-            for (int i = openParen + 1; i < markdown.length(); i ++) {
-                if(markdown.substring(i, i+1).equals("(")) {
-                    countOpenBracket ++;
-                } else if (markdown.substring(i, i+1).equals(")")) {
-                    countOpenBracket --;
-                } 
-                if (countOpenBracket == 0) {
-                    closeParen = i;
-                    break;
-                }
-            }
-            String beforeOpenBracket = "";
-            if (openBracket != 0) {
-                beforeOpenBracket = markdown.substring(openBracket - 1, openBracket);
-            }
-            if (!beforeOpenBracket.equals("!") && (closeBracket == openParen - 1) && (closeBracket != openBracket + 1)) {
-                toReturn.add(markdown.substring(openParen + 1, closeParen));
-            }
-            currentIndex = closeParen + 1;
-        }
-
-        return toReturn;
+        Parser parser = Parser.builder().build();
+        Node node = parser.parse(markdown);
+        LinkVisitor visitor = new LinkVisitor();
+        node.accept(visitor);
+        return visitor.links;
     }
 
     public static void main(String[] args) throws IOException {
@@ -54,5 +20,14 @@ public class MarkdownParse {
         String content = Files.readString(fileName);
         ArrayList<String> links = getLinks(content);
 	    System.out.println(links);
+    }
+}
+
+class LinkVisitor extends AbstractVisitor {
+    ArrayList<String> links = new ArrayList<>();
+    @Override
+    public void visit(Link link) {
+        links.add(link.getDestination());
+        visitChildren(link);
     }
 }
--- TryCommonMark.java
+++ TryCommonMark.java
@@ -5,22 +5,26 @@ import org.commonmark.renderer.html.HtmlRenderer;
 class TryCommonMark {
     public static void main(String[] arg) {
         Parser parser = Parser.builder().build();
-        Node document = parser.parse("This is *Sparta*");
-        HtmlRenderer renderer = HtmlRenderer.builder().build();
-        System.out.println(renderer.render(document));
-        Node node = parser.parse("Example\n=======\n\nSome more text");
-        WordCountVisitor visitor = new WordCountVisitor();
+        LinkVisitor visitor = new LinkVisitor();
         node.accept(visitor);
-        System.out.println(visitor.wordCount);
+        System.out.println(visitor.links);
     }
 }
 
 class WordCountVisitor extends AbstractVisitor {
     int wordCount = 0;
-
     @Override
     public void visit(Text text) {
         wordCount += text.getLiteral().split("\\W+").length;
         visitChildren(text);
+    }
+}
+
+class LinkVisitor extends AbstractVisitor {
+    ArrayList<String> links = new ArrayList<>();
+    @Override
+    public void visit(Link link) {
+        links.add(link.getDestination());
+        visitChildren(link);
     }
 }
